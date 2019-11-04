1230fddb520e1d11d38dc99f7be4ba4e63a552a0
allen bleyle <allenbleyle@allens-MacBook-Air.local>
2019-11-03 18:54:43 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'EricksMagicalLabelGenerator';
+export class AppComponent implements OnInit {
+  public labelForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public buildForm(): void {
+    this.labelForm = this.fb.group({
+      to: ['', Validators.required],
+      from: ['TRAMEVER, INC.', Validators.required],
+      purchaseOrder: ['', Validators.required],
+      dept: ['', Validators.required],
+      boxCount: ['', Validators.required],
+      details: this.fb.array([])
+    });
+  }
+
+  public onBoxCountConfirm(value: any): void {
+    console.log('value', value.value);
+  }
+
+  public onSubmit(form: FormGroup): void {
+    console.log('form submitted', form);
+  }
+
+  private patchDetailsFormArray(): void {
+
+  }
+
+
 }
