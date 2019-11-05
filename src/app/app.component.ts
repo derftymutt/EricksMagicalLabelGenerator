baf5adf4b22424cc8b20ca69d572f3f1bc776166
allen bleyle <allenbleyle@allens-MacBook-Air.local>
2019-11-04 23:42:58 -0800

    add details form array, basic next buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   public labelForm: FormGroup;
-
+  public activeDetailIndex = null;
   constructor(private fb: FormBuilder) {}
 
   public ngOnInit(): void {
     this.buildForm();
+  }
+
+  public get detailsFormArray(): FormArray {
+    return this.labelForm.get('details') as FormArray;
   }
 
   public buildForm(): void {
@@ -25,17 +29,51 @@ export class AppComponent implements OnInit {
     });
   }
 
-  public onBoxCountConfirm(value: any): void {
-    console.log('value', value.value);
+  public onBoxCountConfirm(): void {
+    const boxCount = this.labelForm.get('boxCount').value;
+    this.patchDetailsFormArray(boxCount);
+  }
+
+  public onNextBlank(): void {
+    if (this.isAnotherBoxInCount()) {
+      this.activeDetailIndex++;
+    }
+  }
+
+  public onNextSameInfo(): void {
+    if (this.isAnotherBoxInCount()) {
+      const currentFormValues = this.detailsFormArray.at(this.activeDetailIndex) as FormGroup;
+      const nextFormGroup = this.detailsFormArray.at(this.activeDetailIndex + 1) as FormGroup;
+
+      nextFormGroup.get('thing1').patchValue(currentFormValues.get('thing1').value);
+      nextFormGroup.get('thing2').patchValue(currentFormValues.get('thing2').value);
+      nextFormGroup.get('thing3').patchValue(currentFormValues.get('thing3').value);
+      nextFormGroup.get('thing4').patchValue(currentFormValues.get('thing4').value);
+
+      this.activeDetailIndex++;
+    }
   }
 
   public onSubmit(form: FormGroup): void {
     console.log('form submitted', form);
   }
 
-  private patchDetailsFormArray(): void {
+  private patchDetailsFormArray(boxCount: number): void {
+    for (let i = 0; i < boxCount; i++) {
+      this.detailsFormArray.push(
+        this.fb.group({
+          thing1: ['', Validators.required],
+          thing2: ['', Validators.required],
+          thing3: ['', Validators.required],
+          thing4: ['', Validators.required]
+        })
+      );
+    }
 
+    this.activeDetailIndex = 0;
   }
 
-
+  private isAnotherBoxInCount(): boolean {
+    return this.activeDetailIndex < this.detailsFormArray.length - 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
