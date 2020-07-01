784af6930005a7f09975311f9f790940186889c9
Allen Bleyle <derftymutt@gmail.com>
2020-07-01 12:12:23 -0700

    production apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: ' http://localhost:3000/api'
+  apiUrl: 'http://ericksmagicallabelgenerator-env.eba-vppcp8rg.us-east-2.elasticbeanstalk.com/api'
 };
