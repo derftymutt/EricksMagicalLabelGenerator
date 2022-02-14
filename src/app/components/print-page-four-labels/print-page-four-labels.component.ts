31a50a5232cdbacc2746698f455c437f74462dc6
Allen Bleyle <ableyle@developmentreddinet.net>
2022-02-14 12:15:29 -0800

    more cleanup

--- src/app/components/print-page-four-labels/print-page-four-labels.component.ts
+++ src/app/components/print-page-four-labels/print-page-four-labels.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Label } from 'src/app/models/label';
-import { PdfOptions } from 'src/app/models/pdf-options';
 import { PrintData } from 'src/app/models/print-data';
-import { PdfService } from 'src/app/services/pdf.service';
 
 @Component({
   selector: 'app-print-page-four-labels',
@@ -11,16 +9,4 @@ import { PdfService } from 'src/app/services/pdf.service';
 export class PrintPageFourLabelsComponent {
   @Input() public label: Label;
   @Input() public printData: PrintData;
-  @ViewChild('pdfContainer', { static: true }) public pdfContainer: ElementRef;
-
-  public constructor(private pdfService: PdfService) {}
-
-  public onMakePdf(): void {
-    const options: PdfOptions = {
-      filename: 'tramever-labels.pdf',
-      jsPDF: { unit: 'in', format: [6, 4], orientation: 'portrait' }
-    };
-
-    this.pdfService.makePdf(options, this.pdfContainer.nativeElement);
-  }
 }
